559cd60978f0bc5d4b0578e447bb2bc255fa7e75
KaranaLJX <45736736+KaranaLJX@users.noreply.github.com>
2019-11-25 18:30:42 +0800

    增加数据库util类

--- databasetest/testlitepal/app/src/main/java/com/example/testlitepal/MainActivity.java
+++ databasetest/testlitepal/app/src/main/java/com/example/testlitepal/MainActivity.java
@@ -4,10 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 
 import org.litepal.LitePal;
@@ -15,7 +12,6 @@ import org.litepal.annotation.Column;
 import org.litepal.crud.LitePalSupport;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -23,20 +19,92 @@ import java.io.StringReader;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    public SQLiteDatabase readableDatabase;
-    public DBManager dbHelper;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dbHelper
-                = new DBManager(this);
-        dbHelper.openDatabase();
+        copyDbFile(MainActivity.this, "words.db");
+        LitePal.initialize(this);
+        List<Words> words=LitePal.findAll(Words.class);
+        for(Words words1 :words){
+            Log.d("MainActivity","id is"+words1.getId());
+            Log.d("MainActivity","word is "+words1.getWord());
+            Log.d("MainActivity","chineses is"+words1.getChineses());
+        }
 
 
     }
+
+    //用于项目开始时将words.db拷贝进入 /data/data/packagename/database
+    public void copyDbFile(Context context, String db_name) {
+        SharedPreferences shaf = getSharedPreferences("database", MODE_PRIVATE);
+        InputStream in = null;
+        FileOutputStream out = null;
+        String DATABASECOPIED="databasecopied";
+        //String path = "/data/data/" + context.getPackageName() + "/databases/";
+        File filePath = context.getDatabasePath(db_name);
+        //判读是否已经读入，防止多次拷贝
+        if (!shaf.getBoolean(DATABASECOPIED, false)) {
+            try {
+                in = context.getAssets().open(db_name); // 从assets目录下复制
+                out = new FileOutputStream(filePath);
+                int length = -1;
+                byte[] buf = new byte[1024];
+                while ((length = in.read(buf)) != -1) {
+                    out.write(buf, 0, length);
+                }
+                out.flush();
+                SharedPreferences.Editor editor = getSharedPreferences("database", MODE_PRIVATE)
+                        .edit();
+                editor.putBoolean(DATABASECOPIED, true);
+                editor.apply();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (in != null) in.close();
+                    if (out != null) out.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            //}
+        }
+    }
 }
+class Words extends LitePalSupport {
+
+    @Column(unique = true, defaultValue = "unknown")
+    private Integer id;
+    private String word;
+    private String chineses;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public void setChineses(String chineses) {
+        this.chineses = chineses;
+    }
+
+    public String getChineses() {
+        return chineses;
+    }
+// generated getters and setters.
 
 
+}
