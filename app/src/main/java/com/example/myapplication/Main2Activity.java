284db60bf600d00b811e4cb6f238d01ba9ac09d5
KaranaLJX <45736736+KaranaLJX@users.noreply.github.com>
2019-11-27 14:25:02 +0800

    增加启动程序播放动画功能

---
+++ app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -0,0 +1,51 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.widget.ImageView;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.widget.ImageView;
+public class Main2Activity extends Activity
+{    private ImageView welcomeImg = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        welcomeImg = (ImageView) this.findViewById(R.id.welcome_img);
+        AlphaAnimation anima = new AlphaAnimation(0.1f, 1.0f);
+        anima.setDuration(5000);// 设置动画显示时间
+        welcomeImg.startAnimation(anima);
+        anima.setAnimationListener(new AnimationImpl());
+    }
+    private class AnimationImpl implements AnimationListener {
+        @Override
+        public void onAnimationStart(Animation animation) {
+            welcomeImg.setBackgroundResource(R.drawable.welcome_page);
+        }
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            skip();
+        }
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+    }
+    private void skip() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
+
+
