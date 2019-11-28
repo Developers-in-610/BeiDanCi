ae1d5af80bb00f1366329a734214e6a73dd7f87e
Margaret-how <57360398+Margaret-how@users.noreply.github.com>
2019-11-29 00:55:49 +0800

    限时挑战基本功能

--- app/src/main/java/com/example/myapplication/TimeLimitedChallenge.java
+++ app/src/main/java/com/example/myapplication/TimeLimitedChallenge.java
@@ -21,21 +21,22 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
-public class TimeLimitedChallenge extends AppCompatActivity {
+public class TimeLimitedChallenge extends AppCompatActivity implements View.OnClickListener{
     private TextView timeLimit;
     private ArrayList<Words> al;
     private List<String> chineses;
     private int index;
-    private Button choice1;
-    private Button choice2;
+    Button choice1;
+    Button choice2;
     private int score;
+    private String answer;
     private TextView tvenglish;
     private Words nowword;
-    private int knownum;
+    private int wordnum;
+    private TextView score0;
     public static int totalnum;
-    private Button choice3;
-    private Button choice4;
-    private int fence;
+    Button choice3;
+    Button choice4;
     private volatile boolean exit=false;
 
     private class Counter implements Runnable
@@ -45,7 +46,9 @@ public class TimeLimitedChallenge extends AppCompatActivity {
             timeLimit.setText(msg.what + "s");
             if (msg.what == 0) {
                 // 倒计时结束
-                showDialog("挑战结束，得分：");
+                if(score<=wordnum)
+                showDialog("挑战结束，得分："+score);
+                else showDialog("挑战结束，得分"+wordnum);
             }
         }
     };
@@ -75,16 +78,11 @@ if(!exit)
         AlertDialog.Builder rule=new AlertDialog.Builder(this);
         String string=s;
         rule.setMessage(string);
-        rule.setPositiveButton("再次挑战",click1);
+        //rule.setPositiveButton("再次挑战",click1);
         rule.setNegativeButton("退出挑战",click2);
         rule.create().show();
     }
-    private DialogInterface.OnClickListener click1=new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialogInterface, int i) {
-            startActivity(new Intent(TimeLimitedChallenge.this,TimeLimitedChallenge.class));
-        }
-    };
+
     private DialogInterface.OnClickListener click2=new DialogInterface.OnClickListener() {
         @Override
         public void onClick(DialogInterface dialogInterface, int i) {
@@ -92,22 +90,15 @@ if(!exit)
         }
     };
 
-    public void init() {
-        knownum = 0;
-        totalnum = 0;
-        al = DatabaseUtil.GetALLWord();
-        totalnum = al.size();
-        if (totalnum < 4)
-            showDialog("单词数量不足以进行挑战");
-        else{
-            Collections.shuffle(al);
-            index = 0;
+    public void init(int i) {
+        index=i;
             choice1 = (Button) findViewById(R.id.answer1);
             choice2 = (Button) findViewById(R.id.answer2);
             choice3 = (Button) findViewById(R.id.answer3);
             choice4 = (Button) findViewById(R.id.answer4);
+            score0=(TextView)findViewById(R.id.score);
             tvenglish = (TextView) findViewById(R.id.eng);
-            nowword = al.get(index);
+            nowword = al.get(index%totalnum);
             tvenglish.setText(nowword.getWord());
             chineses = new LinkedList<String>() {{
                 add(nowword.getChineses());
@@ -121,25 +112,59 @@ if(!exit)
             choice2.setText(chineses.get(1));
             choice3.setText(chineses.get(2));
             choice4.setText(chineses.get(3));
-        }
-    }
+            score0.setText("得分:"+score);
 
+    }
+public void nextWord()
+{
+init(++index);
+}
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review_challenge1);
-        init();
+        wordnum = 0;
+        score=0;
+        al = DatabaseUtil.GetALLWord();
+        totalnum = al.size();
+        if (totalnum < 4)
+            showDialog("单词数量不足以进行挑战");
+        else
+            Collections.shuffle(al);
+        init(0);
         timeLimit = (TextView) findViewById(R.id.time);
 
         Counter counter = new Counter();
         Thread counter1 = new Thread(counter, "计时");
         counter1.start();
+        choice1.setOnClickListener(this);
+        choice2.setOnClickListener(this);
+        choice3.setOnClickListener(this);
+        choice4.setOnClickListener(this);
 
 
 
+                }
+                @Override
+    public void onClick(View view)
+                {answer=nowword.getChineses();
 
+                    if(wordnum==totalnum){
+exit=true;
+                        showDialog("挑战结束，得分"+score);
+                        return;
+                    }
+                    switch (view.getId()){
+                        case R.id.answer1:if(answer==choice1.getText().toString())score++;break;
+                        case R.id.answer2:if(answer==choice2.getText().toString())score++;break;
+                        case R.id.answer3:if(answer==choice3.getText().toString())score++;break;
+                        case R.id.answer4:if(answer==choice4.getText().toString())score++;break;
+                        default:break;
+                    }
+                    wordnum++;
+               nextWord();
                 }
 
             }
